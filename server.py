d6aa56088fe606d206c5f3341bbe24c014292384
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2025-05-22 00:33:11 +0200

    Removed threading and finish v1 of Server Object

--- server.py
+++ server.py
@@ -1,37 +1,48 @@
 import socket
-import threading
-
-HOST = "127.0.0.1"
-PORT = 54321
-
 
 class LauncherServer:
     
     def __init__(self, host: str = "127.0.0.1", port: int = 54321):
         self.__host: str = host
         self.__port: int = port
-        self.__server_socket: socket.socket = None
+        self.__server: socket.socket = None
         self.__running = False
         
-
     def handle_client(self, conn: socket, addr: tuple[str, int]):
         print(f"[SERVER] Accepted client connection")
         conn.close()
 
-    def start_server(self):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((HOST, PORT))
-        server.listen()
-        print(f"[SERVER] Server starts listening on {HOST}: {PORT}")
-
+    def accept_clients(self):
         try:
             while True:
-                conn, addr = server.accept()
-                thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-                thread.start()
-                print(f"[SERVER] Active connections: {threading.active_count() - 1}")            
+                conn, addr = self.__server.accept()
+                self.handle_client(conn, addr)
         except KeyboardInterrupt:
-            print(f"[SERVER] Server stops listening on {HOST}: {PORT}")
+            self.stop_server()
 
-            print(f"[SERVER] Server socket closing.")
-            server.close()
+    def start_server(self):
+        if(self.__running):
+            return
+        self.__running = True
+
+        self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server.bind((self.__host, self.__port))
+        self.__server.listen()
+        print(f"[SERVER] Server starts listening on {self.__host}: {self.__port}")
+
+        self.accept_clients()
+
+    def stop_server(self):
+        if(not self.__running):
+            return
+
+        print(f"[SERVER] Server stops listening on {self.__host}: {self.__port}")
+
+        print(f"[SERVER] Server socket closing.")
+        self.__server.close()
+
+        self.__running = False
+
+if __name__ == "__main__":
+    server:LauncherServer = LauncherServer()
+    server.start_server()
