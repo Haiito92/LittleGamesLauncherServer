850fa36851238d0ffc1d0c1078f4aa3eb68c1605
Antoine Hanna <antoine.hanna@op3ft.org>
2025-05-21 14:41:21 +0200

    Start of Server Class creation

--- server.py
+++ server.py
@@ -4,27 +4,33 @@ import threading
 HOST = "127.0.0.1"
 PORT = 54321
 
-def handle_client(conn: socket, addr: tuple[str, int]):
-    print(f"[SERVER] Accepted client connection")
-    conn.close()
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen()
-    print(f"[SERVER] Server starts listening on {HOST}: {PORT}")
+class LauncherServer:
+    
+    def __init__(self, host: str = "127.0.0.1", port: int = 54321):
+        self.__host: str = host
+        self.__port: int = port
+        self.__server_socket: socket.socket = None
+        self.__running = false
 
-    try:
-        while True:
-            conn, addr = server.accept()
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
-            thread.start()
-            print(f"[SERVER] Active connections: {threading.active_count() - 1}")            
-    except KeyboardInterrupt:
-        print(f"[SERVER] Server stops listening on {HOST}: {PORT}")
+    def handle_client(self, conn: socket, addr: tuple[str, int]):
+        print(f"[SERVER] Accepted client connection")
+        conn.close()
 
-        print(f"[SERVER] Server socket closing.")
-        server.close()
+    def start_server(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((HOST, PORT))
+        server.listen()
+        print(f"[SERVER] Server starts listening on {HOST}: {PORT}")
 
-if __name__ == "__main__":
-    start_server()
+        try:
+            while True:
+                conn, addr = server.accept()
+                thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+                thread.start()
+                print(f"[SERVER] Active connections: {threading.active_count() - 1}")            
+        except KeyboardInterrupt:
+            print(f"[SERVER] Server stops listening on {HOST}: {PORT}")
+
+            print(f"[SERVER] Server socket closing.")
+            server.close()
