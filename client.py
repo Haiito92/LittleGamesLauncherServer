de7b74baa3583b7376f1efef49f116563e2b0dbe
Antoine Hanna <antoine.hanna@op3ft.org>
2025-05-21 13:51:37 +0200

    Test simple client connection/deconnection

---
+++ client.py
@@ -0,0 +1,5 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 54321))
+client.close()
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 54321
+
+def handle_client(conn: socket, addr: tuple[str, int]):
+    print(f"[SERVER] Accepted client connection")
+    conn.close()
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,7 +16,10 @@ def start_server():
 
     try:
         while True:
-            pass
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[SERVER] Active connections: {threading.active_count() - 1}")            
     except KeyboardInterrupt:
         print(f"[SERVER] Server stops listening on {HOST}: {PORT}")
 
