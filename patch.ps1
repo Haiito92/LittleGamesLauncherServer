624a94364de7faf7448fd384b990dc3a119c9900
Haiito92 <hannaantoinebusiness@gmail.com>
2025-06-20 19:59:34 +0200

    Bump patch version to 1.0.7 and build

--- patch.ps1
+++ patch.ps1
@@ -1,15 +1,12 @@
 Write-Output "Start of patch update build process."
 Write-Output "Fetching all branches.`n"
 
-cd ../
 git fetch origin
 git checkout master
 
 Write-Output "Merging dev into master..."
 git merge --no-ff origin/dev -m "Merge dev into main - patch update"
 Write-Output "`n"
-
-cd HTTP
 
 Write-Output "`nChecking current build version..."
 $versionFile = "version.txt"
@@ -29,8 +26,6 @@ Write-Output "New version: $newVersion`n"
 Write-Output "Writing new version to: ""$versionFile""..."
 Set-Content -Path $versionFile -Value $newVersion
 Write-Output "New version written.`n"
-
-cd ../
 
 Write-Output "Adding changes..."
 git add .
